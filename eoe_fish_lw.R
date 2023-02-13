bdacb173c7482184773ec68a87df4bb8c83eba0c
weston <westonslaughter@gmail.com>
2023-02-13 10:38:16 -0500

    add porter coho lw reg

--- eoe_fish_lw.R
+++ eoe_fish_lw.R
@@ -76,7 +76,49 @@ plot(x, y, xlab = "Length (mm)", ylab = "Weight (g)")
 title('SF Eel River Pike Minnow Length to Weight\n Wiyot and Stillwater e-Fishing Data')
 curve(a*x^slope, from=0, to=1000, , xlab="x", ylab="y", add = TRUE)
 
-# upload df to gdrive
-googlesheets4::write_sheet(eoe_fish_data,
-                           ss = "https://docs.google.com/spreadsheets/d/1S60cJ4mvPsbA50NsWkZbuwwygiREbqiFxlGubKmHp6A/edit#gid=0",
-                           sheet = 'eoe_snorkel_data')
+# Porter Coho Data?
+porter_coho_ss = 'https://docs.google.com/spreadsheets/d/146BQDKbSbFiLXW53gW51pUsPM5moki0v9-DsXTWy3Wg/edit#gid=0'
+# retrieve metadata of this google sheet, each sub-sheet name and ID
+porter_coho_ss_info <- googlesheets4::gs4_get(porter_coho_ss)
+porter_coho_id <- porter_coho_ss_info$spreadsheet_id
+
+porter_coho_sheet <- googlesheets4::read_sheet(ss = porter_coho_id)
+
+# select datetime, FID, FL, wt
+porter_coho_data <- porter_coho_sheet %>%
+  mutate(
+    weight_g = as.numeric(unlist(wt))
+  ) %>%
+  filter(!is.na(weight_g),
+         !is.na(FL)) %>%
+  select(
+    datetime = date,
+    fish_id = FID,
+    length_mm = FL,
+    weight_g
+  )
+
+# using the 2019 length and weight data, let's fit a power model
+x = porter_coho_data$length_mm
+y = porter_coho_data$weight_g
+
+
+#fit the model
+model <- lm(log(y)~ log(x))
+
+#view the output of the model
+mod_sum = summary(model)
+intercept = mod_sum$coefficients[1,1]
+slope = mod_sum$coefficients[2,1]
+
+# power regression: ln(y) = intercept + slope*ln(x)
+## log(y) = intercept + slope*log(x)
+## y = e ^ intercept + slope * log(x)
+## y = exp(intercept)x^slope
+## y = ax^b
+a = exp(intercept)
+b = slope
+# simplified ax^b equation and plt
+plot(x, y, xlab = "Length (mm)", ylab = "Weight (g)")
+title('Porter Creek, Sonomo Co. CA, O. Kisutch Length to Weight\n e-Fishing Data')
+curve(a*x^slope, from=0, to=1000, , xlab="x", ylab="y", add = TRUE)
